474c56dc9cfa89ba2a7e4835bb5134640208e523
María Vedia <mvtercero85@gmail.com>
2018-09-12 12:49:37 +0200

    Done first logic js

--- main.js
+++ main.js
@@ -1,10 +1,23 @@
+let hasPickedCard = false;
+let firstCard;
+let secondCard;
+
 
 function flipCard() {
-  console.log('he clickado')
-  console.log(this);
-  this.classList.toggle('flip');
+  this.classList.add('flip');
+  if (!hasPickedCard) {
+    //primer click
+    hasPickedCard = true;
+    firstCard = this;
+
+  } else {
+    //segundo click
+    hasPickedCard = false;
+    secondCard = this;
+  }
+
+  console.log(firstCard, secondCard)
 }
 
 let cards = document.querySelectorAll('.memory-card');
-console.log(cards)
 cards.forEach(card => card.addEventListener('click', flipCard));
